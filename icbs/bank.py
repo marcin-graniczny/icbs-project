bc7250d2448de1ee2a50d5d815bf2a61d61000ea
Marcin Graniczny <marcingraniczny@gmail.com>
2022-05-15 09:32:33 +0200

    advanced project

---
+++ icbs/bank.py
@@ -0,0 +1,18 @@
+class Bank:
+    def __init__(self):
+        pass
+
+    def import_clients(self, clients_dict):
+        pass
+
+    def import_accounts(self, accounts_dict):
+        pass
+
+    def money_transfer_transaction(self, amount, from_account, to_account):
+        pass
+
+    def print_transaction_list_for_account(self, iban):
+        pass
+
+    def print_transaction_list_for_all_accounts(self):
+        pass
---
+++ icbs/transaction.py
@@ -0,0 +1,5 @@
+class Transaction:
+    def __init__(self, from_account, to_account, amount):
+        self.from_account = from_account
+        self.to_account = to_account
+        self.amount = amount
